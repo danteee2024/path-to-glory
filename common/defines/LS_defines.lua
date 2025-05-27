7bcf78c3997618fd0aaf99fecb2fd7ae5ab5c8bc
danteee2 <AlfieWatkins2005@outlook.com>
2025-05-27 13:35:33 +0100

    tank. plane, ship cost to edit = 0xp

--- common/defines/LS_defines.lua
+++ common/defines/LS_defines.lua
@@ -141,6 +141,19 @@ NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 0
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 0
 NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 0
 
+
+NDefines.NMilitary.LAND_EQUIPMENT_BASE_COST = 0	-- Cost in XP to upgrade a piece of equipment one level is base + ( total levels * ramp )
+NDefines.NMilitary.LAND_EQUIPMENT_RAMP_COST = 1
+NDefines.NMilitary.NAVAL_EQUIPMENT_BASE_COST = 0
+NDefines.NMilitary.NAVAL_EQUIPMENT_RAMP_COST = 0
+NDefines.NMilitary.AIR_EQUIPMENT_BASE_COST = 0
+NDefines.NMilitary.AIR_EQUIPMENT_RAMP_COST = 0
+
+NDefines.NProduction.EQUIPMENT_MODULE_ADD_XP_COST = 0.0				-- XP cost for adding a new equipment module in an empty slot when creating an equipment variant.
+NDefines.NProduction.EQUIPMENT_MODULE_REPLACE_XP_COST = 0.0				-- XP cost for replacing one equipment module with an unrelated module when creating an equipment variant.
+NDefines.NProduction.EQUIPMENT_MODULE_CONVERT_XP_COST = 0.0				-- XP cost for converting one equipment module to a related module when creating an equipment variant.
+NDefines.NProduction.EQUIPMENT_MODULE_REMOVE_XP_COST = 0.0
+
 NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5
 NDefines.NMilitary.UNIT_EXP_LEVELS = {0.02, 0.04, 0.06, 0.08, 0.1, 0.14, 0.18, 0.22, 0.26, 0.3, 0.39, 0.48, 0.57, 0.66, 0.75, 0.78, 0.81, 0.84, 0.87, 0.9}
 NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 5
