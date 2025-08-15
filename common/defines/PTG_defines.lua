66b5b893628f22720e4dc7756b3b8f1ace81232d
danteee2 <AlfieWatkins2005@outlook.com>
2025-08-15 06:14:36 +0100

    pp per day decrease

--- common/defines/PTG_defines.lua
+++ common/defines/PTG_defines.lua
@@ -113,7 +113,7 @@ NDefines.NResistance.GARRISON_MANPOWER_LOST_BY_ATTACK = 0.005 	--WAS 0.018 Ratio
 
 NDefines.NPolitics.ARMY_LEADER_MAX_COST = 75				-- max cost BEFORE modifiers
 NDefines.NPolitics.NAVY_LEADER_MAX_COST = 75
-NDefines.NPolitics.BASE_POLITICAL_POWER_INCREASE = 2.5 -- base pp gain per day	
+NDefines.NPolitics.BASE_POLITICAL_POWER_INCREASE = 2.25 -- base pp gain per day	
 
 NDefines.NMilitary.SUPPLY_GRACE = 84 -- 72 3 days as they say and remain hungry for another 12
 
